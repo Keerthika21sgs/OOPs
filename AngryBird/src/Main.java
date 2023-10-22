ef8ae5a1b9c39492521a33ef9ad0436fe53e3d86
Keerthika21sgs <81873624+Keerthika21sgs@users.noreply.github.com>
2023-10-22 20:24:38 +0530

    Add files via upload

---
+++ AngryBird/src/Main.java
@@ -0,0 +1,22 @@
+import Model.*;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        BlueBird b1blue = new BlueBird();
+//        b1blue.attack();
+//
+        RedBird r1Red = new RedBird();
+        r1Red.attack();
+
+
+
+//        Bird b1 = new BlueBird();
+//
+//        b1.fly();
+
+
+    }
+}
